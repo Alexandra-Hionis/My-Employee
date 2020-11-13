04ff09975f7c2d32699faca34b4b32fdea37f690
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-11-13 12:34:18 -0500

    added create table function to sql file

--- myEmployeeSchema.sql
+++ myEmployeeSchema.sql
@@ -5,22 +5,22 @@ USE myEmployee_DB;
 
 
 --  Department
-
+CREATE TABLE departments(
 id INT PRIMARY KEY
 name VARCHAR(30) -- to hold department name
-
+);
 -- Role
-
+CREATE TABLE roles(
 id INT PRIMARY KEY
 title VARCHAR(30) -- to hold role title
 salary DECIMAL -- to hold role salary
 department_id INT -- to hold reference to department role belongs to
-
+);
 -- Employee
-
+CREATE TABLE employees(
 id INT PRIMARY KEY
 first_name VARCHAR(30) -- to hold employee first name
 last_name VARCHAR(30) -- to hold employee last name
 role_id INT -- to hold reference to role employee has
 manager_id INT -- to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-
+);
