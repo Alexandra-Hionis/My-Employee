441fda61af4424ec8dca867263a0cae06f8ab77a
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-11-18 21:36:23 -0500

    Only had one manager position. Updated managerid value to just INT instead of INT NOT NULL in employees table. Also added NULL to appropriate value lists in schema

--- myEmployeeSchema.sql
+++ myEmployeeSchema.sql
@@ -22,7 +22,7 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL, -- to hold employee first name
 last_name VARCHAR(30) NOT NULL, -- to hold employee last name
 role_id INT NOT NULL, -- to hold reference to role employee has
-manager_id INT NOT NULL -- to hold reference to another employee that manages the employee being created. This field may be null if the employee has no manager
+manager_id INT -- to hold reference to another employee that manages the employee being created. This field may be null if the employee has no manager
 );
 
 
@@ -30,10 +30,10 @@ INSERT INTO departments (department_name)
 VALUES ('Marketing'), ('Sales'), ('Finance'), ('Human Resources'), ('Operations');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('Marketing Analyst', 55000, 1), ('Sales Manager', 100000, 2), ('Chief Financial Officer', 135600, 3), ('Human Resources Assistant', 35000, 4), ('Operations Assistant', 30000, 5);
+VALUES ('Marketing Analyst', 55000, 1), ('Sales Manager', 100000, 2), ('Chief Financial Officer', 135600, 3), ('Human Resources Assistant', 35000, 4), ('Operations Assistant', 30000, 7);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Mark', 'McGillan', 6, 11), ('Ashley', 'Carrol', 7, 12), ('Sydney', 'Rogers', 8, 13), ('Julia', 'Pete', 9, 14), ('Tom', 'Swayze', 10, 15);
+VALUES ('Mark', 'McGillan', 6, NULL), ('Ashley', 'Carrol', 7, 12), ('Sydney', 'Rogers', 8, NULL), ('Julia', 'Pete', 9, NULL), ('Tom', 'Swayze', 10, NULL);
 
 SELECT * FROM departments
 SELECT * FROM roles
